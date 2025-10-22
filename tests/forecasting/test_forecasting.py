1c9086d81baf34c77c477348e8c06fa34f71d4f6
BimaMalabonga <b.malabonga@det.nl>
2025-10-22 14:30:47 +0200

    wip

---
+++ tests/forecasting/test_forecasting.py
@@ -0,0 +1,29 @@
+"""
+Important notes:
+- Pytest requires test modules to follow the naming convention "test_*.py"
+- Pytest requires test functions to follow the naming convention "test_*()"
+"""
+# Python built-in packages
+from pathlib import Path
+
+# Internal modules
+from detquantlib.forecasting import *
+
+FOLDER_DIR = Path(__file__).parent
+DATA_DIR = FOLDER_DIR.joinpath("_Data")
+
+
+def test_forecast_knife_strategy():
+    # Load mock forecast data
+    df = pd.read_csv(DATA_DIR.joinpath("forecast_knife_strategy_in.csv"))
+    dates = pd.DatetimeIndex(pd.to_datetime(df["date"]))
+    values = df["injection"].values
+
+    # Load expected output
+    expected = np.load(DATA_DIR.joinpath("forecast_knife_strategy_out.npy"))
+
+    # Call function
+    actual = forecast_knife_strategy(dates=dates, values=values)
+
+    # Assert
+    np.testing.assert_array_equal(actual, expected)
