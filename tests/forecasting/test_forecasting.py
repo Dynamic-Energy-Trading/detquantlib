8c33273dc74e5de2c44727dac1c20abbc2de8218
BimaMalabonga <b.malabonga@det.nl>
2025-10-22 14:42:31 +0200

    wip

--- tests/forecasting/test_forecasting.py
+++ tests/forecasting/test_forecasting.py
@@ -13,10 +13,28 @@ FOLDER_DIR = Path(__file__).parent
 DATA_DIR = FOLDER_DIR.joinpath("_Data")
 
 
-def test_forecast_knife_strategy():
-    # Load mock forecast data
+def test_forecast_knife_strategy_timezone_naive():
+    # Load mock forecast data, with timezone-naive dates
     df = pd.read_csv(DATA_DIR.joinpath("forecast_knife_strategy_in.csv"))
     dates = pd.DatetimeIndex(pd.to_datetime(df["date"]))
+    values = df["injection"].values
+
+    # Load expected output
+    expected = np.load(DATA_DIR.joinpath("forecast_knife_strategy_out.npy"))
+
+    # Call function
+    actual = forecast_knife_strategy(dates=dates, values=values)
+
+    # Assert
+    np.testing.assert_array_equal(actual, expected)
+
+
+def test_forecast_knife_strategy_timezone_aware():
+    # Load mock forecast data, with timezone-aware dates
+    df = pd.read_csv(DATA_DIR.joinpath("forecast_knife_strategy_in.csv"))
+    dates = pd.DatetimeIndex(
+        data=pd.to_datetime(df["date"]), tz="Europe/Amsterdam", ambiguous="infer"
+    )
     values = df["injection"].values
 
     # Load expected output
